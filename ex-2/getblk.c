dbf4615c6394d6d1ce7216eb12bdbbebb7b51c13
naoya <mine@mine-studio.com>
2016-10-31 02:32:40 +0900

    getblkコマンドの実装

--- ex-2/getblk.c
+++ ex-2/getblk.c
@@ -10,18 +10,22 @@ getblk(int blkno)
     if ((p = hash_search(blkno)) != NULL) {
       if (p->stat & STAT_LOCKED) {
         /* Scenario 5 */
+        printf("Scenario 5\n");
         p->stat |= STAT_WAITED;
         // sleep(event buffer becomes free);
         printf("Process goes to sleep\n");
         return NULL;
       }
       /* Scenario 1 */
+      printf("Scenario 1\n");
       p->stat |= STAT_LOCKED;
       remove_buffer_from_free_list(p);
       return p;
     } else {
       if (free_is_empty()) {
         /* Scenario 4 */
+        printf("Scenario 4\n");
+        p->stat |= STAT_WAITED;
         // sleep(event any buffer becomes free);
         printf("Process goes to sleep\n");
         return NULL;
@@ -31,11 +35,13 @@ getblk(int blkno)
       p->stat |= STAT_LOCKED;
       if (p->stat & STAT_DWR) {
         /* Scenario 3 */
+        printf("Scenario 3\n");
         p->stat |= STAT_OLD;
         // asynchronous write buffer to disk;
         continue;
       }
       /* Scenario 2 */
+      printf("Scenario 2\n");
       remove_hash(p);
       p->blkno = blkno;
       p->stat &= ~STAT_VALID;
--- ex-2/main.c
+++ ex-2/main.c
@@ -9,6 +9,7 @@
 extern int buffer_size;
 
 extern void getargs(char*, int, int*, char**);
+extern struct buf_header *getblk(int);
 
 extern void init_head();
 extern void init_buffer();
@@ -186,6 +187,20 @@ freecmd(int argc, char** argv)
 void
 getblkcmd(int argc, char** argv)
 {
+  if (argc == 2) {
+    char *endp;
+    int n = strtol(argv[1], &endp, 10);
+    if (*endp == '\0') {
+      struct buf_header *p;
+      if ((p = getblk(n)) == NULL) {
+        fprintf(stderr, "getblk(%d) returns NULL.\n", n);
+      }
+    } else {
+      fprintf(stderr, "Invalid argument: '%s' is not a number.\n", argv[1]);
+    }
+  } else {
+    fprintf(stderr, "Invalid argument: Command '%s' requires 1 parameter. Please see help.\n", argv[0]);
+  }
 }
 
 void
